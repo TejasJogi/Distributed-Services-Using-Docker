3a5b80aa7924914c6d8c3f34d5efcce422d161e0
Tejas Jogi <79081311+TejasJogi@users.noreply.github.com>
2021-10-26 18:51:17 +0530

    Adding HTML code
    This HTML code for uploading files and put it in Upload directory of server.

--- FileUpload/server.py
+++ FileUpload/server.py
@@ -15,6 +15,16 @@ def index():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('index'))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    <p>%s</p>
+    """ % "<br>".join(os.listdir(app.config['UPLOAD_FOLDER'],))
         
 @app.route('/download/<path:path>', methods=['GET', 'POST'])
 def get_files(path):
