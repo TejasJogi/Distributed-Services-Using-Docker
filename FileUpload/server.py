3ba9be4e6f0d8390b8109c77329270c657d94b53
Tejas Jogi <79081311+TejasJogi@users.noreply.github.com>
2021-10-26 17:17:47 +0530

    Update forward service
    This forward service will help to forward that uploaded file to client ,

--- FileUpload/server.py
+++ FileUpload/server.py
@@ -7,7 +7,7 @@ UPLOAD_FOLDER = './Upload'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-
+@app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
@@ -15,6 +15,13 @@ def index():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             return redirect(url_for('index'))
+        
+@app.route('/download/<path:path>', methods=['GET', 'POST'])
+def get_files(path):
+    try:
+        return send_from_directory(UPLOAD_FOLDER, path, as_attachment=True)
+    except FileNotFoundError:
+        print(404))
 
 
 if __name__ == "__main__":
