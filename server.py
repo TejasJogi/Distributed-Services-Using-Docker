61c8dbd5315f1f7e65cd3ee8b4c7206cc1a917b1
Tejas Jogi <79081311+TejasJogi@users.noreply.github.com>
2021-10-26 13:37:50 +0530

    Added Upload service
    This service for upload file to the server

---
+++ server.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './Upload'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run()
