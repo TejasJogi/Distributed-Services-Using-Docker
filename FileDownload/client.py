2c92192c273b46fa194995fbb14146fcbe1c4032
Tejas Jogi <79081311+TejasJogi@users.noreply.github.com>
2021-11-10 12:12:50 +0530

    give path to save downloaded file

--- FileDownload/client.py
+++ FileDownload/client.py
@@ -3,3 +3,6 @@ import requestes
 url = 'http://localhost:5050/download/'
 filename = input("Enter file name: ")
 path = './Download/'
+
+file = open(path + filename, mode='wb').write(response.content)
+print(response.headers)
